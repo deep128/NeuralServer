a1587c6beadc6d7269374a3663b91fc30e7a38de
Deepak Mishra <deepakm157@gmail.com>
2018-02-22 15:16:18 +0530

    Sign up insert

--- src/authService/SignUp.js
+++ src/authService/SignUp.js
@@ -2,18 +2,18 @@
 
 module.exports = function(moduleArg) {
     moduleArg.app.post('/api/signup',function(req, res) {
-        
-        // const sql = `INSERT INTO users Username='${req.query.username}'`;
 
-        // moduleArg.con.query(sql, function (err, result) {
-        //     if (err) throw err;
-        //     res.status(200);
-        //     res.end(JSON.stringify({
-        //         msg: "User not found",
-        //         status: "404"
-        //     }));
-        //   });
-        // res.end("");
+        const sql = `Insert into users (Username, FirstName, LastName, Email, Gender, Password) values ('${req.body.username}', '${req.body.firstname}', '${req.body.lastname}', '${req.body.email}', '${req.body.gender=='male'?1:0}', '${req.body.password}')`;
+
+        moduleArg.con.query(sql, function (err, result) {
+            if (err) throw err;
+            res.status(200);
+            res.end(JSON.stringify({
+                msg: "Inserted",
+                status: "300"
+            }));
+          });
+        res.end("");
     });
 
     moduleArg.app.get('/api/signup/checkusername',function(req,res) {
